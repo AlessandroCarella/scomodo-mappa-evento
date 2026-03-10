21263481d73c69343209e291f6ab7d458bd930c3
AlessandroCarella <alessandro.carella.universita@gmail.com>
2026-03-10 23:53:50 +0100

    init

---
+++ readme.md
@@ -0,0 +1,21 @@
+start:
+* apri cmd in cartella
+* `
+    npm install
+`
+
+* `
+    npm run dev
+`
+
+per check app
+* `
+    npm install
+`
+* `
+    npm run build
+`
+* `
+    npm run preview
+`
+
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// src/App.jsx
+import "./styles/App.css";
+
+function AppContent() {
+    return (
+        <>
+            <div className="app-container mt-4">
+                <p>mappa</p>
+            </div>
+        </>
+    );
+}
+
+function App() {
+    return (
+        <AppContent />
+    );
+}
+
+export default App;
